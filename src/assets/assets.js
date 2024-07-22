58617c18100b33df5fc901b86d4d48f0dab983fa
thanush7 <alaguthanush78@gmail.com>
2024-07-22 10:49:16 +0530

    fsed

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -10,7 +10,7 @@ import facebook from './facebook.png'
 import instagram from './instagram.png'
 import mail from './mail.png'
 
-const url = 'https://gallery-backend-nb096c5ct-thanushs-projects.vercel.app';
+const url = 'https://gallery-backend-8qcqhaqap-thanushs-projects.vercel.app';
 export const assets ={
     logo,
     add_icon,
